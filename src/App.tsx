e9d7b8d3c2f73e97f46bfcb4c9cff81f03801c1d
Lietson Dos Santos <lietsondosantos.dev@gmail.com>
2025-10-02 18:00:38 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect } from 'react'
+import { Detector } from 'detector-js'
+
+type os = { name: string, version: string, versionName: string }
+
 function App() {
+  const detector = new Detector();
+
+  useEffect(() => {
+    if (detector.os && (detector.os as os).name) {
+      const systemDetected = (detector.os as os).name
+
+      if(systemDetected === 'Android') {
+        window.location.replace('https://play.google.com/store/apps/details?id=com.kangwru.app&pli=1');
+      } else if (systemDetected === 'iOS') {
+        window.location.replace('https://apps.apple.com/ao/app/kangwru/id6574340392');
+      } else {
+        window.location.replace('https://kangwru.com');
+      }
+    } else {
+      window.location.replace('https://kangwru.com');
+    }
+  }, [])
+
   return (
     <>
-      <p>Redirect....</p>
+      <p>Redirecting....</p>
     </>
   )
 }
